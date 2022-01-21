24bc214f074beff8a99e495cb14a8575c114c472
MacBook von Lukas <lukas@lukasstrutz.de>
2022-01-21 09:57:15 +0100

    Grundlegende Spielmechanik mit Ausnahme der Gewinnerbestimmung

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -37,6 +38,51 @@ public class Main {
     }
 
     public static void startGame(Spielfeld spielfeld) {
+        //Spielvariablen definieren
         boolean gameRunning = true;
+        boolean inputRunning = true;
+        int currentPlayer = 1;
+        Scanner scanner = new Scanner(System.in);
+
+        //So lange das Spiel läuft neue Runden starten
+        while (gameRunning) {
+
+            while (inputRunning) {
+                //Zuerst Auswählen lassen an welche Stelle der Spieler den Spielstein setzen möchte
+                System.out.println("Wo möchtest du den Spielstein setzen?");
+                System.out.println("Bitte gib eine Zahl zwischen 1 und 7 ein: ");
+                int input = scanner.nextInt();
+
+                //Eingabe überprüfen
+                if (!(0 <= input && input <= 7)) {
+                    //Fehler
+                    //TODO: Fehlermeldung
+                } else {
+                    //Testen, ob an dieser Stelle noch Platz ist,
+                    if (spielfeld.spielfeld[0][input - 1] == 0) {
+                        //wenn noch Platz frei ist, Spielstein setzen.
+                        for (int x = 5; x >= 0; x--) {
+                            if (spielfeld.spielfeld[x][input - 1] == 0) {
+                                spielfeld.spielfeld[x][input - 1] = currentPlayer;
+                                inputRunning = false;
+                                break;
+                            }
+                        }
+                    } else {
+                        //Reihe ist bereits voll
+                        //TODO: Fehlermeldung und erneute Eingabe
+                    }
+                }
+
+            }
+
+            //Testen ob Spieler gewonnen hat
+            //Fall ja, Spiel beenden
+            //Sonst ist der andere Spieler dran
+            Ausgeben.spielfeldausgeben(spielfeld);
+            inputRunning = true;
+            gameRunning = false;
+
+        }
     }
 }
